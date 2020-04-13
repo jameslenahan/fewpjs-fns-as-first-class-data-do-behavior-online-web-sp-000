b0fe8173c3c3b9854b1d004fd0f1cb39f2d3fdbb
James Lenahan <james.k.lenahan@gmail.com>
2020-04-13 20:25:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+function greet(String) {
+  if (time < 12) {
+    return "Good Morning"
+  }
+}
+
 /* Write your implementation of displayMessage() */
